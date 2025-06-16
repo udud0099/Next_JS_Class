cc4536906faed4525d5b1f93d00a507020e1cf3c
udud0099 <udud0099@gmail.com>
2025-06-16 21:33:12 +0545

    project 1 - update

---
+++ Full Project/Project 1 - WebDev Warriors/productlanding_starter_main/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const fonts = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Product Landing Page",
+  description:
+    "Product landing Page using next js, typescript and tailwind css 4",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${fonts.className}  antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ Full Project/Project 1 - WebDev Warriors/productlanding_starter_main/app/page.tsx
@@ -0,0 +1,12 @@
+import Home from "@/components/Home/Home";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default HomePage;
