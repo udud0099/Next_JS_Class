9bdd753efca557717c5b85940a5b4597796e3cb0
udud0099 <udud0099@gmail.com>
2025-07-14 21:59:00 +0545

    making api t36

---
+++ Class form start/class3/src/app/not-found.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const notFound = () => {
+  return <div>notFound - global</div>;
+};
+
+export default notFound;
---
+++ Class form start/class3/src/util/db.js
@@ -0,0 +1,22 @@
+export const user = [
+  {
+    name: "sabita",
+    age: 19,
+    email: "sabita@gmail.com",
+  },
+  {
+    name: "kopila",
+    age: 23,
+    email: "kopila@gmail.com",
+  },
+  {
+    name: "dichhchha",
+    age: 21,
+    email: "dichhchha@gmail.com",
+  },
+  {
+    name: "garima",
+    age: 17,
+    email: "garima@gmail.com",
+  },
+];
