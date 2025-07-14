db8cbca86104b7a4db607c4a6e57c053cdc14586
udud0099 <udud0099@gmail.com>
2025-07-14 10:28:37 +0545

    Next JS tutorial in Hindi #13 Catch-all Segments of Route in Next.js

---
+++ Class form start/class1/src/app/teacher/[...student]/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const allPage = ({ params }) => {
+  return (
+    <div>
+      <h1>allPage is : {params.student}</h1>
+    </div>
+  );
+};
+
+export default allPage;
