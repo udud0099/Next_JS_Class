460f9bd4c10cddec1fe3551ef608e05096778c30
udud0099 <udud0099@gmail.com>
2025-07-15 21:34:42 +0545

    GET data from mongoDB

--- Class form start/class4/src/app/api/[...student]/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(requst, content) {
-  const studentDetails = content.params.student;
-  console.log(studentDetails);
-  return NextResponse.json({ result: studentDetails }, { status: 200 });
-  //   return new Response("all ok");
-}
--- Class form start/class4/src/app/not-found.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const notFound = () => {
-  return <div>notFound - global</div>;
-};
-
-export default notFound;
