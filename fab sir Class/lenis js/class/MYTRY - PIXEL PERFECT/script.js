2c9c62066443f321aa494b37acb9349b417bad2e
udud0099 <udud0099@gmail.com>
2025-08-01 11:59:25 +0545

    update

---
+++ fab sir Class/lenis js/class/MYTRY - PIXEL PERFECT/script.js
@@ -0,0 +1,5 @@
+
+const headerMenu = document.querySelector(".header")
+const burgerMenu = headerMenu.querySelector(".burger")
+const headerBackdrop = headerMenu.querySelector(".header-backdrop")
+const closeMenu = headerMenu.querySelector(".close-menu")
